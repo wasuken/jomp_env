096492d39ab814eb0e8bc322f3117343bba28247
wasuken <wevorence@gmail.com>
2024-05-09 21:16:30 +0900

    修正

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -22,12 +22,12 @@ export const authConfig: NextAuthConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const isOnDashboard = nextUrl.pathname.startsWith("/user");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        return Response.redirect(new URL("/user/dashboard", nextUrl));
       }
       return true;
     },
